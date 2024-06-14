a3a8c8c9ccf2052d7e3dc6473a1f924875636b8f
minzion <aert12435@gmail.com>
2024-06-14 20:09:57 +0900

    project set

---
+++ src/main/java/com/example/bookmaru/BookmaruApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmaruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmaruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookmaru/BookmaruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmaru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmaruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
