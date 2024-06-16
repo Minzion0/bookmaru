05008f4d8b899811c8e2304de4e81b1fe87a8037
minzion <aert12435@gmail.com>
2024-06-17 00:23:16 +0900

    readme

--- src/main/java/com/example/bookmaru/BookmaruApplication.java
+++ src/main/java/com/example/bookmaru/BookmaruApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookmaruApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BookmaruApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BookmaruApplication.class, args);
+  }
 
 }
