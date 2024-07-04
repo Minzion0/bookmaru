24dd0a079bfe8570735de4cda766882f4d24a873
minzion <aert12435@gmail.com>
2024-07-04 21:32:47 +0900

    토큰 준비

--- src/main/java/com/example/bookmaru/domain/repository/MemberRepository.java
+++ src/main/java/com/example/bookmaru/domain/repository/MemberRepository.java
@@ -1,2 +1,12 @@
-package com.example.bookmaru.domain.repository;public interface MemberRepository {
+package com.example.bookmaru.domain.repository;
+
+import com.example.bookmaru.domain.model.entity.MemberEntity;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<MemberEntity,Long> {
+  Optional<MemberEntity> findByEmail(String email);
+
 }
